30aaf98eaa745d914adade9dc4058e8ea38f04a3
jhg98 <57996692+jhg98@users.noreply.github.com>
2025-07-09 00:28:51 +0900

    원격 플레이어 입력 비활성화 테스트

---
+++ Assets/VRProject/Jeong/Scripts_Jeong/Player/DisableRemoteXRController.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DisableRemoteXRController : MonoBehaviourPun
+{
+    public XRBaseController gazeInteractor;
+    public XRBaseController leftController;
+    public XRBaseController rightController;
+
+    void Awake()
+    {
+        if (!photonView.IsMine)
+        {
+            gazeInteractor.enabled = false;
+            leftController.enabled = false;
+            rightController.enabled = false;
+        }
+    }
+}
