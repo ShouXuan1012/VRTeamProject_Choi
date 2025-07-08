dba45f98cb8247c497c41d501907fb1b2728e71f
ehgus2390 <165624422+ehgus2390@users.noreply.github.com>
2025-07-08 17:56:35 +0900

    test2
    test2

--- Assets/VRProject/Jeong/Scripts_Jeong/PlayerSpawner.cs
+++ Assets/VRProject/Jeong/Scripts_Jeong/PlayerSpawner.cs
@@ -1,14 +1,16 @@
 ﻿using Photon.Pun;
 using UnityEngine;
 
-// 테스트용
 public class PlayerSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private Transform spawnPoint;
 
-    private void Start()
+    void Start()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+        }
     }
 }
