81034fe62ec819631ffdf6a92a0b2f9c2e35898c
jhg98 <57996692+jhg98@users.noreply.github.com>
2025-07-08 22:50:44 +0900

    플레이어 프리팹 분리, 카메라 테스트

---
+++ Assets/VRProject/Jeong/Scripts_Jeong/Player/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class CameraController : MonoBehaviourPun
+{
+    public Camera playerCamera;
+
+    void Start()
+    {
+        if (photonView.IsMine)
+            playerCamera.gameObject.SetActive(true);
+    }
+}
