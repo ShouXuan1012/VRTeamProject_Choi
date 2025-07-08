171e68a1681ea009e5d9c06cba17e1418046b06d
ShouXuan1012 <cuishouxuan@gmail.com>
2025-07-08 15:14:37 +0900

    농구장 구현중

---
+++ Assets/VRProject/Choi/Scripts_Choi/VRAvatarIKController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class VRAvatarIKController : MonoBehaviour
+{
+    public Transform leftHandTarget;
+    public Transform rightHandTarget;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if (animator == null) return;
+
+        // ¿Þ¼Õ
+        if (leftHandTarget != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTarget.position);
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTarget.rotation);
+        }
+
+        // ¿À¸¥¼Õ
+        if (rightHandTarget != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandTarget.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandTarget.rotation);
+        }
+    }
+}
