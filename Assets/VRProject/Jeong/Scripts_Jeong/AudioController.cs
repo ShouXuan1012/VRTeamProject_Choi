c23aee3970255d52e952d021a99752016f2cf183
jhg98 <57996692+jhg98@users.noreply.github.com>
2025-07-10 14:47:37 +0900

    필요없는 코드 삭제

--- Assets/VRProject/Jeong/Scripts_Jeong/AudioController.cs
+++ Assets/VRProject/Jeong/Scripts_Jeong/AudioController.cs
@@ -17,29 +17,20 @@ public class AudioController : MonoBehaviourPunCallbacks
     }
     private void Start()
     {
-        Debug.Log("방 들어옴");
         if (PhotonNetwork.IsMasterClient)
         {
-            Debug.Log("마스터임");
             musicStartTime = (float)PhotonNetwork.Time;
             photonView.RPC("PlayAudio", RpcTarget.All, musicStartTime);
 
-            // 마스터가 바뀔 경우 사용할 변수를 룸 속성에 저장
             Hashtable props = new Hashtable { { "MusicStartTime", musicStartTime } };
             PhotonNetwork.CurrentRoom.SetCustomProperties(props);
         }
         else
         {
-            Debug.Log("마스터 아님");
-            // 다른 플레이어는 마스터가 설정한 음악 시작 시간을 가져옴
             if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("MusicStartTime", out object startTime))
             {
                 musicStartTime = (float)startTime;
                 photonView.RPC("PlayAudio", PhotonNetwork.LocalPlayer, musicStartTime);
-            }
-            else
-            {
-                Debug.LogWarning("음악 시작 시간이 설정되지 않았습니다.");
             }
         }
     }
@@ -54,7 +45,6 @@ public class AudioController : MonoBehaviourPunCallbacks
     [PunRPC]
     public void PlayAudio(float startTime)
     {
-        Debug.Log("PlayAudio RPC 호출됨, 시작 시간: " + startTime);
         if (musicSource != null && !musicSource.isPlaying)
         {
             float timeSinceStart = (float)PhotonNetwork.Time - startTime; // 경과 시간
