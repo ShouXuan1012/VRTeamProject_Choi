ceda2f2461ec87077e7323ca0b5e7c1bfc064192
ShouXuan1012 <cuishouxuan@gmail.com>
2025-07-10 17:16:37 +0900

    플레이어 구현중

--- Assets/VRProject/Choi/Scripts_Choi/VRMovementAnimator.cs
+++ Assets/VRProject/Choi/Scripts_Choi/VRMovementAnimator.cs
@@ -8,7 +8,6 @@ public class VRMovementAnimator : MonoBehaviourPun
     [SerializeField] private float inputThreshold = 0.05f;
 
     private Animator animator;
-    private bool wasMoving = false;
 
     private void Start()
     {
@@ -22,20 +21,6 @@ public class VRMovementAnimator : MonoBehaviourPun
         Vector2 moveInput = moveProvider.leftHandMoveAction.action.ReadValue<Vector2>();
         bool isMoving = moveInput.magnitude > inputThreshold;
 
-        if (isMoving != wasMoving)
-        {
-            animator.SetBool("isMoving", isMoving); // 내 애니메이션
-            photonView.RPC("SetMovingAnim", RpcTarget.Others, isMoving); // 다른 사람에게 동기화
-            wasMoving = isMoving;
-        }
-    }
-
-    [PunRPC]
-    public void SetMovingAnim(bool isMoving)
-    {
-        if (photonView.IsMine) return; // 내 캐릭터는 직접 처리했으므로 제외
-        if (animator == null) animator = GetComponent<Animator>();
-
-        animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isMoving", isMoving); // 내 로컬 애니메이션 (동기화는 자동)
     }
 }
