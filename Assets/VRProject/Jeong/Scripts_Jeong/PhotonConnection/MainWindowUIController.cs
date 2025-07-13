62212e4ef1a38583247f66b40149404f07881368
jhg98 <57996692+jhg98@users.noreply.github.com>
2025-07-13 16:43:40 +0900

    시작화면 UI 연결 작업
    로직 연결, UI 구조 수정, UI 컨트롤 방식 변경

---
+++ Assets/VRProject/Jeong/Scripts_Jeong/PhotonConnection/MainWindowUIController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum LobbyState
+{
+    Default,
+    Loading,
+    Ready,
+    Error
+}
+public class MainWindowUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject menuScreen;
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject errorScreen;
+
+    [SerializeField] private Button joinRoomButton;
+    [SerializeField] private Button quitButton;
+
+    [SerializeField] private Text errorMessage;
+
+    /// <summary>
+    /// 로비 상태에 따라 UI 설정
+    /// </summary>
+    /// <param name="state"></param>
+    public void SetUIByLobbyState(LobbyState state)
+    {
+        switch (state)
+        {
+            case LobbyState.Default:
+                menuScreen.SetActive(true);
+                loadingScreen.SetActive(false);
+                errorScreen.SetActive(false);
+
+                joinRoomButton.interactable = false;
+                quitButton.interactable = true;
+                break;
+
+            case LobbyState.Loading:
+                menuScreen.SetActive(false);
+                loadingScreen.SetActive(true);
+                errorScreen.SetActive(false);
+                break;
+
+            case LobbyState.Ready:
+                menuScreen.SetActive(true);
+                loadingScreen.SetActive(false);
+                errorScreen.SetActive(false);
+
+                joinRoomButton.interactable = true;
+                quitButton.interactable = true;
+                break;
+
+            case LobbyState.Error:
+                menuScreen.SetActive(false);
+                loadingScreen.SetActive(false);
+                errorScreen.SetActive(true);
+                break;
+        }
+    }
+    /// <summary>
+    /// 에러 메세지 업데이트
+    /// </summary>
+    /// <param name="message"></param>
+    public void SetErrorMessage(string message)
+    {
+        if (errorMessage != null)
+        {
+            errorMessage.text = message;
+        }
+    }
+}
