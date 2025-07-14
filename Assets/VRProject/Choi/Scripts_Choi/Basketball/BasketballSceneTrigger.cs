3d14f793817d9f4b296d3c80c07f0bbb321816f9
ShouXuan1012 <cuishouxuan@gmail.com>
2025-07-14 15:42:44 +0900

    농구장 구현중

--- Assets/VRProject/Choi/Scripts_Choi/Basketball/BasketballSceneTrigger.cs
+++ Assets/VRProject/Choi/Scripts_Choi/Basketball/BasketballSceneTrigger.cs
@@ -1,16 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BasketballSceneTrigger : MonoBehaviour
+public class BasketballSceneTrigger : MonoBehaviourPunCallbacks
 {
+    private bool hasRequested = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (hasRequested) return;
+        if (!other.CompareTag("Player")) return;
+
+        PhotonView view = other.GetComponent<PhotonView>();
+        if (view != null && view.IsMine)
         {
-            SceneManager.LoadScene("Basketball Court_Choi");
+            hasRequested = true;
+            PhotonNetwork.LeaveRoom(); // ·ë ³ª°¡±â
         }
+    }
+
+    public override void OnLeftRoom()
+    {
+        Debug.Log("Left room. Waiting for Master Server...");
+        StartCoroutine(WaitForMasterThenCreateRoom());
+    }
+
+    private IEnumerator WaitForMasterThenCreateRoom()
+    {
+        while (!PhotonNetwork.IsConnectedAndReady)
+        {
+            yield return null;
+        }
+
+        string roomName = "Basketball_" + PhotonNetwork.LocalPlayer.NickName + "_" + Random.Range(1000, 9999);
+        PhotonNetwork.CreateRoom(roomName, new Photon.Realtime.RoomOptions { MaxPlayers = 1 });
+    }
+
+    public override void OnCreatedRoom()
+    {
+        // ·ë »ý¼ºµÇ¸é ³ó±¸Àå ¾ÀÀ¸·Î ÀÌµ¿
+        SceneManager.LoadScene("Basketball Court_Choi");
     }
 }
