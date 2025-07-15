bf36a2b0dd641e445096e7d10aa6dd5ee2266794
xkxk01 <bggmh.h01@gmail.com>
2025-07-15 16:58:58 +0900

    250715_17:00
    UI노출 방식 변경
    UI카메라 따로 설정

--- Assets/VRProject/Cha/Scripts_Cha/Items/ItemDetector.cs
+++ Assets/VRProject/Cha/Scripts_Cha/Items/ItemDetector.cs
@@ -3,22 +3,19 @@ using static UnityEditor.Progress;
 
 public class ItemDetector : MonoBehaviour
 {
-    public MagnifierUI magnifierUI; // 돋보기 UI 참조
-    public float maxDistance = 5f;  // 감지 거리 조절
-    public float holdDuration = 0.3f;  // 벗어나도 유지되는 시간
-    public float sphereRadius = 0.1f;  // 감지 반지름
-    public LayerMask itemLayerMask;
-   
+    public float maxDistance = 5f;             // 감지 거리
+    public float holdDuration = 0.3f;          // 유지 시간
+    public float sphereRadius = 0.1f;          // 감지 반지름
+    public LayerMask itemLayerMask;            // 감지 대상 레이어
 
     private InspectableItem currentItem;
     private float lostTime = 0f;
 
     void Update()
     {
-        
         Ray ray = new Ray(transform.position, transform.forward);
-       
         Debug.DrawRay(ray.origin, ray.direction * maxDistance, Color.red);
+
         if (Physics.SphereCast(ray, sphereRadius, out RaycastHit hit, maxDistance, itemLayerMask))
         {
             var item = hit.collider.GetComponent<InspectableItem>();
@@ -26,25 +23,33 @@ public class ItemDetector : MonoBehaviour
             {
                 if (item != currentItem)
                 {
-                    Debug.Log("새로운 아이템 감지됨: " + item.name);
+                    // 이전 아이템 UI 비활성화
+                    if (currentItem != null)
+                    {
+                        currentItem.magnifierUI.ClearTarget();
+                        currentItem.magnifierUI.gameObject.SetActive(false);
+                    }
+
                     currentItem = item;
-                    magnifierUI.gameObject.SetActive(true);
-                    magnifierUI.SetTarget(item);
-                    
+
+                    currentItem.magnifierUI.gameObject.SetActive(true);
+                    currentItem.magnifierUI.SetTarget(currentItem);
                 }
+
                 lostTime = 0f;
                 return;
             }
         }
 
+        // 감지되지 않음
         if (currentItem != null)
         {
             lostTime += Time.deltaTime;
             if (lostTime > holdDuration)
             {
+                currentItem.magnifierUI.ClearTarget();
+                currentItem.magnifierUI.gameObject.SetActive(false);
                 currentItem = null;
-                magnifierUI.ClearTarget();
-                magnifierUI.gameObject.SetActive(false);
             }
         }
     }
