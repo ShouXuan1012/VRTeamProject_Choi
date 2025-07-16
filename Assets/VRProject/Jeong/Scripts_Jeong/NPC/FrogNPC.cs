72e7f883c7d395b3ffb463ba73627d6cecd688d8
jhg98 <57996692+jhg98@users.noreply.github.com>
2025-07-16 17:54:53 +0900

    NPC 작업 중

--- Assets/VRProject/Jeong/Scripts_Jeong/NPC/FrogNPC.cs
+++ Assets/VRProject/Jeong/Scripts_Jeong/NPC/FrogNPC.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+처음엔 NPC 캐릭터 위에 물음표 말풍선 띄우기
+가까이 다가가면 NPC 외곽선 띄우고 클릭할 수 있게? 아님 그냥 말풍선 클릭할 수 있게 활성화
+클릭하면 소개 말풍선 띄우기, NPC 캐릭터가 플레이어를 바라보게 만들기
+말풍선 클릭하면 다음 대화로 넘어가기
+텍스트는 한번에 보여주는 게 아니라 한 글자씩 보여주기
+멀어지면 다시 물음표 말풍선 띄우기
+다시 가까이 다가가면 처음 대화 상태로 말풍선 띄우기
+끝까지 읽으면 말풍선 닫고 '...' 말풍선 띄우기
+ */
 public class FrogNPC : MonoBehaviour
 {
     // Start is called before the first frame update
