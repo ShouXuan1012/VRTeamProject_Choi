37a1cd700ac505972c0017c21281e502d6ed8da8
SuHyeon <cuishouxuan@gmail.com>
2025-07-17 21:00:46 +0900

    플레이어 테스트

--- Assets/VRProject/Choi/Scripts_Choi/Player/RemoteTransformSync.cs
+++ Assets/VRProject/Choi/Scripts_Choi/Player/RemoteTransformSync.cs
@@ -1,38 +1,44 @@
 using UnityEngine;
 using Photon.Pun;
 
-// 자신을 제외한 다른 플레이어들에게 자신의 Transform을 동기화하는 스크립트
 public class RemoteTransformSync : MonoBehaviourPun, IPunObservable
 {
+    [Header("전송 여부")]
+    [SerializeField] private bool syncPosition = true;
+    [SerializeField] private bool syncRotation = true;
+
+    [Header("부드럽게 보간")]
+    [SerializeField] private float lerpSpeed = 10f;
+
     private Vector3 networkPosition;
     private Quaternion networkRotation;
-
-    public float lerpSpeed = 10f;
 
     void Update()
     {
         if (photonView.IsMine) return;
 
-        transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * lerpSpeed);
-        transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.deltaTime * lerpSpeed);
+        if (syncPosition)
+        {
+            transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * lerpSpeed);
+        }
+
+        if (syncRotation)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.deltaTime * lerpSpeed);
+        }
     }
 
-    // PhotonView가 소유한 객체의 상태를 네트워크로 전송하거나 수신하는 데 사용되는 메서드
-    // IsWriting: 현재 객체가 데이터를 보내는지 여부
-    // IsReading: 현재 객체가 데이터를 받는지 여부
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
-            // 자신의 Transform 정보를 네트워크로 전송
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
+            if (syncPosition) stream.SendNext(transform.position);
+            if (syncRotation) stream.SendNext(transform.rotation);
         }
         else
         {
-            // 다른 플레이어로부터 Transform 정보를 수신
-            networkPosition = (Vector3)stream.ReceiveNext();
-            networkRotation = (Quaternion)stream.ReceiveNext();
+            if (syncPosition) networkPosition = (Vector3)stream.ReceiveNext();
+            if (syncRotation) networkRotation = (Quaternion)stream.ReceiveNext();
         }
     }
 }
