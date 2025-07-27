1a72e38554d051cddc54726565e2fa93d2ce6a1a
jhg98 <57996692+jhg98@users.noreply.github.com>
2025-07-27 23:15:48 +0900

    로그인 ui 처리

--- Assets/VRProject/Jeong/Scripts_Jeong/Login/LoginManager.cs
+++ Assets/VRProject/Jeong/Scripts_Jeong/Login/LoginManager.cs
@@ -20,67 +20,64 @@ public class LoginManager : MonoBehaviour
 
     public async void HandleLogin()
     {
-        // 예: 사용자 인증, 데이터베이스 조회 등
         string loginId = loginIdInputField.text;
         string loginPassword = loginPasswordInputField.text;
 
         if (!IsValidId(loginId))
         {
-            Debug.LogError($"아이디가 유효하지 않습니다.: {loginId}");
+            uiController.SetLoginNoticeMessage($"아이디가 유효하지 않습니다.");
             return;
         }
         if (!IsValidPassword(loginPassword))
         {
-            Debug.LogError("비밀번호가 유효하지 않습니다.");
+            uiController.SetLoginNoticeMessage("비밀번호가 유효하지 않습니다.");
             return;
         }
 
-        // 로딩 표시 활성화
+        uiController.SetUIByLoginState(LoginState.Loading);
 
         isIdExists = await UserDataManager.Instance.CheckIdExists(loginId);
         if (!isIdExists)
         {
-            Debug.LogError($"로그인 실패: 아이디가 존재하지 않습니다.: {loginId}");
-            // 에러 메시지 표시 후 로그인 창으로 돌아가기
+            uiController.SetLoginNoticeMessage("아이디가 존재하지 않습니다.");
+            uiController.SetUIByLoginState(LoginState.Error);
             return;
         }
         else
         {
-            // 아이디가 존재하는 경우, 사용자 데이터를 가져와서 비밀번호 확인
+            // 아이디가 존재하는 경우 사용자 데이터를 가져와서 비밀번호 확인
             UserData userData = await UserDataManager.Instance.GetUserData(loginId);
             if (userData != null && userData.password == loginPassword)
             {
-                Debug.Log("로그인 성공");
-                // 메인 메뉴 창 활성화
+                uiController.SetUIByLoginState(LoginState.Success);
             }
             else
             {
-                Debug.LogError("로그인 실패: 비밀번호가 잘못되었습니다.");
-                // 에러 메시지 표시 후 로그인 창으로 돌아가기
+                uiController.SetLoginNoticeMessage("비밀번호가 일치하지 않습니다.");
+                uiController.SetUIByLoginState(LoginState.Error);
             }
         }
     }
 
     public async void HandleSignUp()
     {
-        // 예: 사용자 정보 입력, 데이터베이스 저장 등
         string signUpId = signUpIdInputField.text;
         string signUpPassword = signUpPasswordInputField.text;
 
         if (!IsValidId(signUpId))
         {
-            Debug.LogError($"아이디가 유효하지 않습니다.: {signUpId}");
+            uiController.SetSignUpNoticeMessage("아이디가 유효하지 않습니다.");
             return;
         }
         if (!IsValidPassword(signUpPassword))
         {
-            Debug.LogError("비밀번호가 유효하지 않습니다.");
+            uiController.SetSignUpNoticeMessage("비밀번호가 유효하지 않습니다.");
             return;
         }
 
         if (isIdExists)
         {
-            Debug.LogError("아이디가 이미 존재합니다. 다른 아이디를 사용해주세요.");
+            uiController.SetSignUpNoticeMessage("아이디가 이미 존재합니다.\n다른 아이디를 사용해주세요.");
             return;
         }
 
@@ -94,18 +91,18 @@ public class LoginManager : MonoBehaviour
         };
 
         // 로딩 표시 활성화
+        uiController.SetUIBySignUpState(SignUpState.Loading);
 
         bool isSaved = await UserDataManager.Instance.SaveUserData(newUser);
         if (!isSaved)
         {
-            Debug.LogError("회원가입 실패: 사용자 데이터를 저장하는 중 오류 발생");
-            // 에러 메시지 표시 후 회원가입 창으로 돌아가기
+            uiController.SetSignUpNoticeMessage("회원가입 중 오류가 발생했습니다.\n다시 시도해주세요.");
+            uiController.SetUIBySignUpState(SignUpState.Error);
             return;
         }
         else
         {
-            Debug.Log("회원가입 성공");
-            // 회원가입 성공 메시지 표시 후 닉네임 창 활성화
+            uiController.SetUIBySignUpState(SignUpState.Success);
         }
     }
 
@@ -115,23 +112,29 @@ public class LoginManager : MonoBehaviour
 
         if (!IsValidId(signUpId))
         {
-            Debug.LogError($"아이디가 유효하지 않습니다.: {signUpId}");
+            uiController.SetSignUpNoticeMessage("아이디가 유효하지 않습니다.");
             return;
         }
 
-        // 로딩 표시 활성화
+        uiController.SetUIBySignUpState(SignUpState.IdChecking);
 
         isIdExists = await UserDataManager.Instance.CheckIdExists(signUpId);
         if (isIdExists)
         {
-            Debug.LogError("아이디가 이미 존재합니다. 다른 아이디를 사용해주세요.");
-            // 아이디 중복 메시지 표시
+            uiController.SetSignUpNoticeMessage("이미 존재하는 아이디입니다.\n다른 아이디를 사용해주세요.");
+            uiController.SetUIBySignUpState(SignUpState.IdNotChecked);
         }
         else
         {
-            Debug.Log("아이디 사용 가능");
-            // 회원가입 버튼 활성화
+            uiController.SetSignUpNoticeMessage("사용 가능한 아이디입니다.", false);
+            uiController.SetUIBySignUpState(SignUpState.IdChecked);
         }
+    }
+
+    private void OnSignUpIdChanged(string newId)
+    {
+        isIdExists = true; // 입력값이 변경되면 중복 여부 초기화
+        uiController.SetUIBySignUpState(SignUpState.IdNotChecked);
     }
 
     private bool IsValidId(string id)
@@ -175,11 +178,5 @@ public class LoginManager : MonoBehaviour
         }
 
         return isValid;
-    }
-
-    private void OnSignUpIdChanged(string newId)
-    {
-        isIdExists = true; // 아이디가 변경되면 중복 여부 초기화
-        // 회원가입 버튼 활성화
     }
 }
