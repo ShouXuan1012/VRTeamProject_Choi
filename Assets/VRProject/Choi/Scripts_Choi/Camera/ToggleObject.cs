0b90e2919a40eb46fa40808d31be1551620362c0
ShouXuan1012 <cuishouxuan@gmail.com>
2025-08-04 11:41:42 +0900

    플레이어 수정중

--- Assets/VRProject/Choi/Scripts_Choi/Camera/ToggleObject.cs
+++ Assets/VRProject/Choi/Scripts_Choi/Camera/ToggleObject.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class ToggleObject : MonoBehaviour
+public class NetworkToggleObject : MonoBehaviourPun
 {
     [SerializeField] private GameObject target;
 
     public void Toggle()
     {
+        if (!photonView.IsMine) return; // 내 플레이어만 토글 가능
+
+        bool newState = !target.activeSelf;
+        photonView.RPC(nameof(RPC_ToggleObject), RpcTarget.All, newState);
+    }
+
+    [PunRPC]
+    void RPC_ToggleObject(bool state)
+    {
         if (target != null)
-            target.SetActive(!target.activeSelf);
+            target.SetActive(state);
     }
 }
