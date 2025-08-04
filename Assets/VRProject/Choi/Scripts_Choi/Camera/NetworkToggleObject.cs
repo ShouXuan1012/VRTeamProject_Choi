3b5e06e7a4c6238206736c4a68d684cdd0115efa
ShouXuan1012 <cuishouxuan@gmail.com>
2025-08-04 12:00:02 +0900

    Test

--- Assets/VRProject/Choi/Scripts_Choi/Camera/NetworkToggleObject.cs
+++ Assets/VRProject/Choi/Scripts_Choi/Camera/NetworkToggleObject.cs
@@ -10,7 +10,7 @@ public class NetworkToggleObject : MonoBehaviourPun
         if (!photonView.IsMine) return; // 내 플레이어만 토글 가능
 
         bool newState = !target.activeSelf;
-        photonView.RPC(nameof(RPC_ToggleObject), RpcTarget.All, newState);
+        photonView.RPC(nameof(RPC_ToggleObject), RpcTarget.AllBuffered, newState);
     }
 
     [PunRPC]
